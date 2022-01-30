ba2a0176572d7d85fe9e5e55a6f56f9d8449be7b
Enrique Franco <luisfrancoz1@gmail.com>
2022-01-30 15:28:36 -0500

    Ref Validation of format for telephone, name, email and birthday has been added

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,39 @@
 class Contact < ApplicationRecord
   belongs_to :user
   validates_presence_of :address, :birthday, :credit_card, :email, :franchise, :name, :phone
+
+  # validates date format to ISO 8601
+  DATE_FORMAT_SLASH = %r'(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[0-1]|0[1-9]|[1-2][0-9])'
+  # DATE_FORMAT_HYPHEN = /^(-?(?:[1-9][0-9]*)?[0-9]{4})\/(1[0-2]|0[1-9])\/(3[0-1]|0[1-9]|[1-2][0-9])$/
+  DATE_FORMAT_HYPHEN = %r'(-?(?:[1-9][0-9]*)?[0-9]{4})\/(1[0-2]|0[1-9])\/(3[0-1]|0[1-9]|[1-2][0-9])'
+  VALID_DATE_FORMAT = /\A(#{DATE_FORMAT_SLASH}|#{DATE_FORMAT_HYPHEN})\z/
+  validates :birthday, format: {
+    with: VALID_DATE_FORMAT,
+    message: 'The date format for birthday must be ISO 8601 %YYYY-%MM-%DD'
+  }
+
+  validates :email, format: {
+    with: Devise.email_regexp,
+    message: 'The email is invalid please check the format'
+  }
+
+  validates :name, format: {
+    with: %r'\A[a-zA-Z\s\-]+\z',
+    message: 'The name format is invalid, names only contains letter, the - is the uniq special character allow'
+  },
+  length: {
+    minimum: 3,
+    maximum: 255,
+    message: "The name must be between 3 to 255 characters"
+  }
+
+  PHONE_NUMBER_FORMAT_ONE = %r'\(\+[0-9]{2}\)\s[0-9]{3}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}\s[0-9]{2}'
+  PHONE_NUMBER_FORMAT_TWO = %r'\(\+[0-9]{2}\)\s[0-9]{3}\s[0-9]{3}\s[0-9]{2}\s[0-9]{2}'
+  VALID_PHONE_FORMAT = /\A(#{PHONE_NUMBER_FORMAT_ONE}|#{PHONE_NUMBER_FORMAT_TWO})\z/
+  validates :phone, format: {
+    with: VALID_PHONE_FORMAT,
+    message: "The phone number format is invalid, allowed format are (+00) 000 000 00 00 00 or (+00) 000 000 00 00"
+  }
+
+
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -4,12 +4,57 @@ RSpec.describe Contact, type: :model do
 
   describe 'validations' do
     it { should validate_presence_of(:address) }
-    it { should validate_presence_of(:birthday) }
+
+    context 'Brithday' do
+      it { should validate_presence_of(:birthday) }
+      it { should_not allow_value('321564').for(:birthday) }
+      it { should_not allow_value('ADSDSF5464').for(:birthday) }
+      it { should_not allow_value('SDK;lkjsd07980987').for(:birthday) }
+      it { should allow_value('2021/02/02').for(:birthday) }
+      it { should allow_value('2021-02-02').for(:birthday) }
+    end
+
     it { should validate_presence_of(:credit_card) }
-    it { should validate_presence_of(:email) }
+
+    context 'Email' do
+      it { should validate_presence_of(:email) }
+      it { should_not allow_value('algo.com').for(:email) }
+      it { should_not allow_value('@prueba.com').for(:email) }
+      it { should_not allow_value('prueba.prueba.com').for(:email) }
+      it { should allow_value('prueba@prueba.com').for(:email) }
+      it { should allow_value('prueba12355443@prueba.com').for(:email) }
+      it { should allow_value('prueba.ejemplo@prueba.com').for(:email) }
+      it { should allow_value('prueba_ejemplo@prueba.com').for(:email) }
+    end
+
     it { should validate_presence_of(:franchise) }
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:phone) }
+
+    context 'Name' do
+      it { should validate_presence_of(:name) }
+      it { should_not allow_value('a').for(:name) }
+      it { should_not allow_value('a2').for(:name) }
+      it { should_not allow_value('Andy Whinehows4').for(:name) }
+      it { should_not allow_value('a' * 256).for(:name) }
+      it { should_not allow_value('Charles *(&^(*&hjhghjgjkh))').for(:name) }
+      it { should allow_value('John Doe').for(:name) }
+      it { should allow_value('Robert-Lawrence').for(:name) }
+    end
+
+    context 'Telephone' do
+      it { should validate_presence_of(:phone) }
+      it { should_not allow_value('3106180000').for(:phone) }
+      it { should_not allow_value('+573106180000').for(:phone) }
+      it { should_not allow_value('(+57)3106180000').for(:phone) }
+      it { should_not allow_value('(+57)310 6180000').for(:phone) }
+      it { should_not allow_value('(+57)310 618 0000').for(:phone) }
+      it { should_not allow_value('(+57) 310 618 0000').for(:phone) }
+      it { should_not allow_value('(+57) 31O 618 00 00').for(:phone) }
+      it { should_not allow_value('(+57) 310 618 00 A4').for(:phone) }
+      it { should allow_value('(+57) 310 618 00 00').for(:phone) }
+      it { should allow_value('(+57) 310 618 00 00 00').for(:phone) }
+    end
+
+
   end
 
   describe 'relationships' do
